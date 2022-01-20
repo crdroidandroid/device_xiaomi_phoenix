240ab0837427ac1757ed14d37b39083ce1810684
SharmagRit <sharmagrit04@gmail.com>
2022-01-20 18:58:11 +0000

    phoenix: It's time to CrDroid

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_phoenix.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_phoenix-user \
+    lineage_phoenix-userdebug \
+    lineage_phoenix-eng
+
